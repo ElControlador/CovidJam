469b60fb9485dd87b1916a2d0341c5e82a2c3146
ManupaDogflau <63118529+ManupaDogflau@users.noreply.github.com>
2020-04-05 01:12:34 +0200

    ges
    ge

---
+++ CovidJam Project/Assets/Repetir.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Repetir : MonoBehaviour
+{
+    // Start is called before the first frame 
+    private float counter = 0;
+    private Scene p;
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter >= 2)
+        {
+            p = SceneManager.GetActiveScene();
+
+            SceneManager.LoadScene(p.name);
+        }
+    }
+}
