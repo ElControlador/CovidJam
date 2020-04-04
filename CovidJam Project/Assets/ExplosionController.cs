cacabd41cf38d2ca0d50c12b56fa34ff03b8adcf
afgordillo <afgordillo@xone.es>
2020-04-04 11:18:58 +0200

    - Bomb

---
+++ CovidJam Project/Assets/ExplosionController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionController : MonoBehaviour
+{
+    public void endAnimation()
+    {
+        transform.parent.gameObject.SetActive(false);
+    }
+}
--- CovidJam Project/Assets/LinkController.cs
+++ CovidJam Project/Assets/LinkController.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class LinkController : MonoBehaviour
 {
     HingeJoint2D hingeJoint;
+    BoxCollider2D boxCollider2D;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag.Equals("Ball"))
         {
             hingeJoint.enabled = false;
+            StartCoroutine(deshabilitar());
+            // boxCollider2D.enabled = false;
         }
+    }
+
+    internal void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Explosion"))
+        {
+            hingeJoint.enabled = false;
+            StartCoroutine(deshabilitar());
+            // boxCollider2D.enabled = false;
+        }
+    }
+
+    private IEnumerator deshabilitar()
+    {
+        yield return new WaitForSeconds(1f);
+        gameObject.SetActive(false);
     }
 
     public void Awake()
     {
         hingeJoint = GetComponent<HingeJoint2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 }
