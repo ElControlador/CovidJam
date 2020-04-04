3e8a946b8ff16f40f485b129f11bf3ba05f0c9ac
ManupaDogflau <63118529+ManupaDogflau@users.noreply.github.com>
2020-04-04 19:57:55 +0200

    gr
    grs

---
+++ CovidJam Project/Assets/Giro.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Giro : MonoBehaviour
+{
+    public Vector3 Position;
+    public float ffd;
+    private Transform punto;
+    private LaunchProjectile launchproyectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        launchproyectile = GameObject.Find("Arma").GetComponent<LaunchProjectile>();
+        punto = GameObject.Find("Punto").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Position = launchproyectile.direction;
+        ffd = Vector3.Angle(punto.position, Position);
+        transform.rotation = Quaternion.Euler(0, 0, Vector3.Angle(punto.position, Position));
+    }
+}
---
+++ CovidJam Project/Assets/Muerte2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muerte2 : MonoBehaviour
+{
+    public GameObject muerto;
+    private GameObject Arma;
+    private Transform personaje;
+    private bool hecho = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Arma = GameObject.Find("Arma");
+        personaje = GameObject.Find("Personaje").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            if (hecho)
+            {
+                Instantiate(muerto, personaje.position, Quaternion.identity);
+                Destroy(gameObject);
+                Destroy(Arma);
+                hecho = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+
+    {
+        if (collision.gameObject.CompareTag("Explosion"))
+        {
+            if (hecho)
+            {
+                Instantiate(muerto, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+                Destroy(Arma);
+                hecho = false;
+            }
+        }
+    }
+}
