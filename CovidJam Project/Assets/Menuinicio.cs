f11869967d31be70c2e1dfb1983ab0f58dedaef3
ManupaDogflau <63118529+ManupaDogflau@users.noreply.github.com>
2021-01-29 14:13:01 +0100

    bug solve
    restart game

--- CovidJam Project/Assets/Menuinicio.cs
+++ CovidJam Project/Assets/Menuinicio.cs
@@ -24,7 +24,7 @@ public class Menuinicio : MonoBehaviour
     {
         p = SceneManager.GetActiveScene();
         n = p.buildIndex;
-        SceneManager.LoadScene(n + 1);
+        SceneManager.LoadScene("nivel1");
     }
     
     public void Howtoplay()
